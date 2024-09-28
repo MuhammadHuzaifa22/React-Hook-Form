379c55219169caba5e51b145295f55febb3ea5b8
MuhammadHuzaifa22 <huzaifafurqan22@gmail.com>
2024-09-28 22:50:31 +0500

    60% work completed

--- src/Pages/admissionForm.jsx
+++ src/Pages/admissionForm.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+// Importing functions and variables.
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { db, storage } from "../Config/firebaseConfig";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { collection, addDoc } from "firebase/firestore";
 
+// Form Submit Component
 const AdmissionForm = () => {
+  let [userEmail, setUserEmail] = useState("");
+  let [userPhoto, setUserPhoto] = useState("");
+  let [submitBtnLoadingCondition, setSubmitBtnLoadingCondition] =
+    useState(false);
+  let [selectedValue, setSelectedValue] = useState("male");
+  let [haveUserLaptop, setHaveUserLaptop] = useState("yes");
+  let [userLastQualification, setUserLastQualification] = useState("matric");
+
+  const Navigate = useNavigate();
+
+  // Use this for React Hook Form
   const {
     register,
     handleSubmit,
@@ -10,9 +26,90 @@ const AdmissionForm = () => {
     formState: { errors },
   } = useForm();
 
+  // Get user gender
+  function getUserGender(event) {
+    setSelectedValue(event.target.value);
+  }
+
+  // Get is user have a laptop?
+  function getUserHaveLaptop(event) {
+    console.log(`before: ${haveUserLaptop}`);
+    setHaveUserLaptop(event.target.value);
+    console.log(event.target.value);
+    console.log(`After: ${haveUserLaptop}`);
+  }
+
+// Get user last qualification
+  function getUserLastQualification(event){
+  setUserLastQualification(event.target.value);
+  }
+
+
   // Form function admit user
-  function admitUser(data) {
+  async function admitUser(data) {
+    const form = document.getElementById("form");
     console.log(data);
+    if (data.email !== "" || data.email !== null) {
+      setSubmitBtnLoadingCondition(true);
+      setUserEmail(data.email);
+      showUrl(data.profileImage[0], data.email);
+      const userImage = await showUrl(data.profileImage[0], data.email);
+      console.log(userImage);
+      console.log(selectedValue);
+      if (userImage !== "" || userImage !== null) {
+        data.userImage = userImage;
+        data.userGender = selectedValue;
+        data.haveUserLaptop = haveUserLaptop;
+        data.userLastQualification = userLastQualification;
+        console.log(data);
+      }
+      sendDataToFiresstore(data);
+      setUserPhoto(userImage);
+    } else {
+      setSubmitBtnLoadingCondition(false);
+    }
+
+    // Reset all form after submit.
+    form.reset();
+  }
+
+  // Send form submitter data to firestore
+  async function sendDataToFiresstore(data) {
+    console.log(data);
+    console.log(data.haveUserLaptop);
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        fullname: data.fullname,
+        fathername: data.fathername,
+        email: data.email,
+        cnic: data.cnic,
+        address: data.address,
+        dateofbirth: data.dateofbirth,
+        phonenumber: data.phonenumber,
+        userProfile: data.userImage,
+        userGender: selectedValue,
+        haveUserLaptop: haveUserLaptop,
+        userLastQualification:userLastQualification
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
+  // Firebase storage show URL function image to link converter
+  async function showUrl(files, email) {
+    const storageRef = ref(storage, email);
+    try {
+      const uploadImg = await uploadBytes(storageRef, files);
+      const url = await getDownloadURL(storageRef);
+      setSubmitBtnLoadingCondition(false);
+      console.log(url);
+      Navigate(`/admission`);
+      return url;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -33,7 +130,8 @@ const AdmissionForm = () => {
         </div>
         <h1 className="text-3xl font-bold text-center  mb-8">Admission Form</h1>
 
-        <form onSubmit={handleSubmit(admitUser)}>
+        {/* Submission Form */}
+        <form onSubmit={handleSubmit(admitUser)} id="form">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {/* Full Name */}
             <div className="flex flex-col">
@@ -44,9 +142,20 @@ const AdmissionForm = () => {
                 className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
                 type="text"
                 placeholder="Full Name"
-                {...register("fullname", { required: true })}
+                {...register("fullname", {
+                  required: true,
+                  pattern: {
+                    value: /^[A-Z][a-z]*.*$/,
+                  },
+                })}
               />
-              {errors.fullname && <span className="text-red-500">Full Name is required.</span>}
+              {errors.fullname && (
+                <span className="text-red-500">
+                  {errors.fullname.type === "required"
+                    ? "Full Name is required."
+                    : "First letter must be capitalized."}
+                </span>
+              )}
             </div>
 
             {/* Father Name */}
@@ -58,9 +167,21 @@ const AdmissionForm = () => {
                 className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
                 type="text"
                 placeholder="Father Name"
-                {...register('fathername',{required:true})}
+                {...register("fathername", {
+                  required: true,
+                  pattern: {
+                    value: /^[A-Z][a-z]*.*$/,
+                  },
+                })}
               />
-              {errors.fathername && <span className="text-red-500">Father Name is rquired.</span>}
+
+              {errors.fathername && (
+                <span className="text-red-500">
+                  {errors.fathername.type === "required"
+                    ? "Father Name is required."
+                    : "First letter must be capitalized."}
+                </span>
+              )}
             </div>
 
             {/* Email */}
@@ -72,9 +193,11 @@ const AdmissionForm = () => {
                 className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
                 type="email"
                 placeholder="Email"
-                {...register('email',{required:true})}
+                {...register("email", { required: true })}
               />
-              {errors.email && <span className="text-red-500">Email is required.</span>}
+              {errors.email && (
+                <span className="text-red-500">Email is required.</span>
+              )}
             </div>
 
             {/* Phone Number */}
@@ -84,11 +207,32 @@ const AdmissionForm = () => {
               </label>
               <input
                 className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
-                type="text"
+                type="number"
                 placeholder="Phone Number"
-                {...register('phonenumber',{required:true})}
+                {...register("phonenumber", {
+                  required: true,
+                  minLength: 11,
+                  maxLength: 11,
+                  pattern: {
+                    value: "/^03d{8}$/",
+                    message:
+                      "Phone Number must start with '03' and be 11 digits long.",
+                  },
+                })}
               />
-              {errors.phonenumber && <span className="text-red-500">Phone Number is required.</span>}
+              {errors.phonenumber && (
+                <span className="text-red-500">
+                  {errors.phonenumber.type === "required"
+                    ? "Phone Number is required."
+                    : errors.phonenumber.type === "minLength"
+                    ? "Phone Number length must be equal to 11."
+                    : errors.phonenumber.type === "maxLength"
+                    ? "Phone Number length must be equal to 11."
+                    : errors.phonenumber.type === "pattern"
+                    ? "Phone Number must starts with 03"
+                    : "Phone Number should meet the prevous requirements."}
+                </span>
+              )}
             </div>
 
             {/* National ID */}
@@ -100,9 +244,27 @@ const AdmissionForm = () => {
                 className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
                 type="number"
                 placeholder="CNIC"
-                {...register('cnic',{required:true})}
+                {...register("cnic", {
+                  required: true,
+                  minLength: 13,
+                  maxLength: 13,
+                })}
               />
-              {errors.cnic && <span className="text-red-500">CNIC is required.</span>}
+              {errors.cnic && errors.cnic.type === "required" && (
+                <span className="text-red-500">CNIC is required.</span>
+              )}
+
+              {errors.cnic && errors.cnic.type === "minLength" && (
+                <span className="text-red-500">
+                  CNIC must be at least 13 characters long.
+                </span>
+              )}
+
+              {errors.cnic && errors.cnic.type === "maxLength" && (
+                <span className="text-red-500">
+                  CNIC must be exactly 13 characters long.
+                </span>
+              )}
             </div>
 
             {/* Date of Birth */}
@@ -113,43 +275,46 @@ const AdmissionForm = () => {
               <input
                 className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
                 type="date"
-                {...register('dateofbirth',{required:true})}
+                {...register("dateofbirth", { required: true })}
               />
-              {errors.dateofbirth && <span className="text-red-500">Date of Birth is required.</span>}
+              {errors.dateofbirth && (
+                <span className="text-red-500">Date of Birth is required.</span>
+              )}
             </div>
 
             {/* Gender */}
-<div className="flex flex-col">
-  <label className="mb-1 " htmlFor="gender">
-    Gender
-  </label>
-  <select
-    className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
-    id="gender"
-    name="gender"
-  >
-   
-    <option value="male">Male</option>
-    <option value="female">Female</option>
-  </select>
-</div>
-
+            <div className="flex flex-col">
+              <label className="mb-1 " htmlFor="gender">
+                Gender
+              </label>
+              <select
+                className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
+                id="gender"
+                name="gender"
+                value={selectedValue}
+                onChange={getUserGender}
+              >
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+            </div>
 
             {/* Laptop */}
-<div className="flex flex-col">
-  <label className="mb-1" htmlFor="laptop">
-    Do you have a Laptop?
-  </label>
-  <select
-    className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
-    id="laptop"
-    name="laptop"
-  >
-    <option value="yes">Yes</option>
-    <option value="no">No</option>
-  </select>
-</div>
-
+            <div className="flex flex-col">
+              <label className="mb-1" htmlFor="laptop">
+                Do you have a Laptop?
+              </label>
+              <select
+                className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
+                id="laptop"
+                name="laptop"
+                value={haveUserLaptop}
+                onChange={getUserHaveLaptop}
+              >
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+              </select>
+            </div>
 
             {/* Address */}
             <div className="flex flex-col col-span-2">
@@ -160,28 +325,31 @@ const AdmissionForm = () => {
                 className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
                 type="text"
                 placeholder="Address"
-                {...register('address',{required:true})}
+                {...register("address", { required: true })}
               />
-              {errors.address && <span className="text-red-500">Address is required.</span>}
+              {errors.address && (
+                <span className="text-red-500">Address is required.</span>
+              )}
             </div>
 
             {/* Last Qualification */}
-<div className="flex flex-col col-span-2">
-  <label className="mb-1" htmlFor="qualification">
-    Last Qualification
-  </label>
-  <select
-    className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
-    id="qualification"
-    name="qualification"
-  >
-    <option value="matric">Matric</option>
-    <option value="intermediate">Intermediate</option>
-    <option value="bachelors">Bachelors</option>
-    <option value="masters">Masters</option>
-  </select>
-</div>
-
+            <div className="flex flex-col col-span-2">
+              <label className="mb-1" htmlFor="qualification">
+                Last Qualification
+              </label>
+              <select
+                className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
+                id="qualification"
+                name="qualification"
+                value={userLastQualification}
+                onChange={getUserLastQualification}
+              >
+                <option value="matric">Matric</option>
+                <option value="intermediate">Intermediate</option>
+                <option value="bachelors">Bachelors</option>
+                <option value="masters">Masters</option>
+              </select>
+            </div>
 
             {/* Profile Image */}
             <div className="flex flex-col col-span-2">
@@ -192,10 +360,14 @@ const AdmissionForm = () => {
                 className="border border-gray-300 p-2 rounded-lg focus:ring focus:ring-offset-2 focus:ring-pastel"
                 type="file"
                 id="profileImage"
-                {...register('profileImage',{required:true,minLength:13,maxLength:13})}
+                {...register("profileImage", {
+                  required: true,
+                })}
               />
-             
-              {errors.profileImage && <span className="text-red-500">Profile Image is required.</span>}
+
+              {errors.profileImage && (
+                <span className="text-red-500">Profile Image is required.</span>
+              )}
             </div>
 
             <div className="col-span-2">
@@ -208,10 +380,18 @@ const AdmissionForm = () => {
             </div>
 
             {/* Submit Button */}
+
             <div className="col-span-2">
-              <button className="btn btn-primary w-full  text-white py-3 rounded-lg hover:bg-indigo-700 transition duration-300">
-                Submit
-              </button>
+              {submitBtnLoadingCondition ? (
+                <button className="btn btn-primary w-full  text-white py-3 rounded-lg hover:bg-indigo-700 transition duration-300">
+                  <span className="loading loading-spinner"></span>
+                  Submitting
+                </button>
+              ) : (
+                <button className="btn btn-primary w-full  text-white py-3 rounded-lg hover:bg-indigo-700 transition duration-300">
+                  Submit
+                </button>
+              )}
             </div>
           </div>
         </form>
